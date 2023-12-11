02dee9aa269059c20f1af048eec5f9b0f0b7f4dc
CaoToan <caotoan940@gmail.com>
2023-12-11 05:40:56 +0100

    Connect to Database

--- index.ts
+++ index.ts
@@ -1,14 +1,27 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import * as database from "./config/database";
+import Article from "./models/article.model";
+
+
 const app: Express = express();
 
 // .env
 dotenv.config();
 
+// database
+database.connect();
+
 const port: string | number = process.env.PORT || 8100;
 
 app.get("/", async (req: Request, res: Response): Promise<void> => {
-    res.send("ok");
+    const articles = await Article.find({
+        deleted: false
+    });
+
+    res.json({
+        articles
+    });
 });
 
 app.listen(port, () => {
